c8732666a9038d423b24381e2457772285392bf4
bjmendez <bjmendez@buffalo.edu>
2016-06-06 15:54:24 -0500

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -6,7 +6,7 @@ import matplotlib
 matplotlib.use("tkAgg")
 import matplotlib.pyplot as plt
 import scipy.fftpack
-
+from scipy import signal
 
 
 Tk().withdraw()
@@ -15,16 +15,16 @@ filename = askopenfilename()
 fields =['period', 'time','HR']
 df = pd.read_csv(filename, usecols=fields)
 ddf = df.dropna()
+
 #plt.show( ddf.plot(kind = 'scatter', x = 'time' , y = 'HR', c = 'c'))
 ddf = ddf[ddf.period == '10/3/2014']
-heatmap, xedges, yedges = np.histogram2d(ddf['time'], ddf['HR'], bins =(150,42))
+ddf = ddf.drop_duplicates()
+heatmap, xedges, yedges = np.histogram2d(ddf['time'], ddf['HR'], bins =(150,30))
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
-print (ddf)
+#print (ddf)
 
 plt.clf()
-plt.imshow(heatmap,extent=extent, origin = 'lower',aspect = 'auto',interpolation='nearest')
+plt.imshow(heatmap.T,extent=extent,aspect = 'auto',interpolation='nearest')
 plt.colorbar()
 plt.show()
-
-
