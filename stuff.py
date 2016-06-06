152be433bd0b21153aa2c0df2ea56168b042fcab
bjmendez <bjmendez@buffalo.edu>
2016-06-06 15:14:54 -0500

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -12,9 +12,19 @@ import scipy.fftpack
 Tk().withdraw()
 filename = askopenfilename()
 
-fields =['period','time','HR']
+fields =['period', 'time','HR']
 df = pd.read_csv(filename, usecols=fields)
+ddf = df.dropna()
+#plt.show( ddf.plot(kind = 'scatter', x = 'time' , y = 'HR', c = 'c'))
+ddf = ddf[ddf.period == '10/3/2014']
+heatmap, xedges, yedges = np.histogram2d(ddf['time'], ddf['HR'], bins =(150,42))
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
-print df
+print (ddf)
+
+plt.clf()
+plt.imshow(heatmap,extent=extent, origin = 'lower',aspect = 'auto',interpolation='nearest')
+plt.colorbar()
+plt.show()
 
 
