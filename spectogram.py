16ce67da14edb6796466594beeed14e2db19724b
bjmendez <bjmendez@buffalo.edu>
2016-06-09 15:13:11 -0500

    Add files via upload
    spectorgram with overlapping attempt

---
+++ spectogram.py
@@ -0,0 +1,37 @@
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use("tkAgg")
+import matplotlib.pyplot as plt
+import scipy.fftpack
+from scipy import signal
+
+
+Tk().withdraw()
+filename = askopenfilename()
+fields =['period', 'time','HR','BR', 'activity', 'ambulation']
+df = pd.read_csv(filename, usecols = fields)
+ddf = df
+
+
+ddf = ddf[ddf.period == '10/4/2014']
+
+
+NFFT = 50
+Fs = 2
+x = 0;
+y= 0.5;
+
+for i in range(48):
+    tempddf = ddf
+    tempddf = tempddf[tempddf.time >= x]
+    tempddf = tempddf[tempddf.time <= y]
+    Pxx, freqs, bins, im = plt.specgram(tempddf['HR'], NFFT=NFFT, window= np.hamming(50), pad_to=NFFT, noverlap=45, Fs=Fs)
+    #plt.colorbar()
+    plt.savefig('pic'+ str(i) + '.png', bbox_inches='tight', pad_inches = 0)
+    #plt.show()
+    x = x + 0.5
+    y = y + 0.5
+
